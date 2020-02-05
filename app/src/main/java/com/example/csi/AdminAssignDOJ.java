1dcd915e662cdf55aa5de6708d0155c906b9c26d
Shibin <shibin340>
2020-02-05 12:55:18 +0530

    Admin updated

--- app/src/main/java/com/example/csi/AdminAssignDOJ.java
+++ app/src/main/java/com/example/csi/AdminAssignDOJ.java
@@ -9,13 +9,16 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Calendar;
+import java.util.StringTokenizer;
 
 public class AdminAssignDOJ extends AppCompatActivity {
     EditText doj_email;
     TextView doj_date;
     Button assign_doj;
+    DatabaseHelper db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +26,7 @@ public class AdminAssignDOJ extends AppCompatActivity {
         doj_email = findViewById(R.id.doj_email);
         doj_date = findViewById(R.id.doj_date);
         assign_doj = findViewById(R.id.assign_doj_bt);
+        db = new DatabaseHelper(getApplicationContext());
         Calendar calendar = Calendar.getInstance();
         final int year = calendar.get(Calendar.YEAR);
         final int month = calendar.get(Calendar.MONTH);
@@ -45,7 +49,30 @@ public class AdminAssignDOJ extends AppCompatActivity {
         assign_doj.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                StringTokenizer s_arr = new StringTokenizer(doj_date.getText().toString().trim(),"/",false);
+                if(doj_email.getText().toString().trim().equals(""))
+                    Toast.makeText(getApplicationContext(),"Please enter email",Toast.LENGTH_SHORT).show();
+                else if(s_arr.countTokens()!=3)
+                    Toast.makeText(getApplicationContext(),"Enter a DOJ",Toast.LENGTH_SHORT).show();
+                else
+                {
+                    if(!db.checkmail(doj_email.getText().toString().trim()) && db.checkapproval(doj_email.getText().toString().trim()))
+                    {
+                        boolean update = db.addDateOfJoining(doj_email.getText().toString().trim(),doj_date.getText().toString().trim());
+                        if(update)
+                        {
+                            Toast.makeText(getApplicationContext(),"Assigned successfully",Toast.LENGTH_SHORT).show();
+                            doj_email.setText(null);
+                            doj_date.setText("Date");
+                            doj_date.setTextColor(getResources().getColor(R.color.hintColor));
+                        }
+                        else
+                            Toast.makeText(getApplicationContext(),"Assigned unsuccessfully",Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        Toast.makeText(getApplicationContext(),"Enter a vslid email",Toast.LENGTH_SHORT).show();
+                    }
+                }
             }
         });
     }
--- app/src/main/java/com/example/csi/AdminAssignEmpID.java
+++ app/src/main/java/com/example/csi/AdminAssignEmpID.java
@@ -3,12 +3,49 @@ package com.example.csi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class AdminAssignEmpID extends AppCompatActivity {
-
+    EditText empid_email,emp_id_et;
+    Button assign_emp_id;
+    DatabaseHelper db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_assign_emp_id);
+        empid_email = findViewById(R.id.empid_email);
+        emp_id_et = findViewById(R.id.emp_id);
+        assign_emp_id = findViewById(R.id.assign_emp_id);
+        db = new DatabaseHelper(getApplicationContext());
+        assign_emp_id.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(empid_email.getText().toString().trim().equals(""))
+                    Toast.makeText(getApplicationContext(),"Enter email",Toast.LENGTH_SHORT).show();
+                else if(emp_id_et.getText().toString().trim().equals(""))
+                    Toast.makeText(getApplicationContext(),"Enter employee ID",Toast.LENGTH_SHORT).show();
+                else
+                {
+                    if(!db.checkmail(empid_email.getText().toString().trim()) && db.checkapproval(empid_email.getText().toString().trim()))
+                    {
+                        boolean update = db.addEmpID(empid_email.getText().toString().trim(),emp_id_et.getText().toString().trim());
+                        if(update)
+                        {
+                            Toast.makeText(getApplicationContext(),"Assigned successfully",Toast.LENGTH_SHORT).show();
+                            empid_email.setText(null);
+                            emp_id_et.setText(null);
+                        }
+                        else
+                            Toast.makeText(getApplicationContext(),"Assigned unsuccessfully",Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        Toast.makeText(getApplicationContext(),"Enter a vslid email",Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
     }
 }
